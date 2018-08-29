d99a879e11d97117fb3054b1a20d66b51a7cb991
Maxwell Carlson <bart4282819@gmail.com>
2018-08-28 22:17:43 -0700

    Looking into using data read online from one of the many sites providing it. Might also use CSV downloaded data for training.. Going to write a data reader that will be a standalone project eventually as I keep needing to write one for each of these projects

--- QPred/QPred.py
+++ QPred/QPred.py
@@ -1,1 +1,17 @@
+import cntk
+import numpy as np
+import matplotlib.pyplot as plt
 
+# Pandas datareader error workaround 
+import pandas as pd
+pd.core.common.is_list_like = pd.api.types.is_list_like
+from pandas_datareader import webr
+
+# Looking into an online data reader. 
+tickers = ['APPL']
+startDate = '2010-01-01'
+endDate = '2016-12-31'
+panelData = webr.DataReader('INPX', 'google', startDate, endDate)
+
+def train():
+    return
